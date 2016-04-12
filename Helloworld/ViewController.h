dbfeeacf6bf6c1a1cd990b096cef4ac3d5601822
Lijinz <lijinb.kunju@gmail.com>
2016-04-12 14:33:51 +0530

    Initial Commit

---
+++ Helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Helloworld
+//
+//  Created by Cybraum on 4/12/16.
+//  Copyright © 2016 Cybraum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Helloworld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Helloworld
+//
+//  Created by Cybraum on 4/12/16.
+//  Copyright © 2016 Cybraum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
